58bb06fc71b97fde26ba319a278bd291d61bd323
wangjianquanwjq <610743994@qq.com>
2022-08-24 17:28:27 +0800

    新增弹幕

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
 // 删除动画,定时器
-// onMounted(() => {
+onMounted(() => {
 //   // @ts-ignore
 //   if (animateTimer) clearInterval(animateTimer);
-//   if (document.querySelector(".first-loading"))
-//     document.querySelector(".first-loading")!.remove();
-// });
+  if (document.querySelector(".first-loading"))
+    document.querySelector(".first-loading")!.remove();
+});
 
 </script>
 <style lang="less">
 body {
   margin: 0;
-  overflow: hidden;
+  // overflow: hidden;
 }
 </style>
--- src/views/homeMap.vue
+++ src/views/homeMap.vue
@@ -1,6 +1,17 @@
 <template>
-  <div style="width:100%;height:100vh" id="container"></div>
+
+  <div style="width:100%;height:100vh" id="container">
+    <vue-danmaku v-model:danmus="danmus" ref="danmakuRef" use-slot loop :fontSize="50" extraStyle="color:red"
+      style="height:100vh; width:100wh">
+      <template slot="dm" v-slot:dm="{ index, danmu }">
+        <div>
+          <el-avatar :src="danmu.avatar"> </el-avatar>{{ danmu.text }}
+        </div>
+      </template>
+    </vue-danmaku>
+  </div>
   <!-- <Drawer /> -->
+
 </template>
 
 <script lang="ts" setup>
@@ -8,12 +19,20 @@ import { isConditional } from '@babel/types';
 import { onMounted, ref, reactive, defineComponent } from 'vue'
 import Drawer from "@/components/Drawer.vue";
 import { createSocket } from "@/utils/WebSocket";
+import vueDanmaku from 'vue3-danmaku'
 let data = [
-  [102.54, 30.05, { type: 1 }],
-  [103.54, 30.95, { type: 2 }],
-  [105.54, 30.95, { type: 2 }],
-  [104.54, 30.95, { type: 2 }],
-  [104.54, 31.05, { type: 1 }]
+  [117.2, 39.13, { type: 1 }],
+  [106.27, 38.47, { type: 2 }],
+  [117.27, 31.86, { type: 1 }],
+  [117, 36.65, { type: 2 }],
+  [112.53, 37.87, { type: 1 }],
+  [121.48, 31.22, { type: 2 }],
+  [102.73, 25.04, { type: 1 }],
+  [111.65, 40.82, { type: 2 }],
+  [116.46, 39.92, { type: 1 }],
+  [125.35, 43.88, { type: 2 }],
+  [104.06, 30.67, { type: 1 }],
+  [104.94, 30.57, { type: 2 }],
 ]
 let icon = new AMap.Icon({
   size: new AMap.Size(60, 60),
@@ -29,11 +48,22 @@ let icon1 = new AMap.Icon({
 });
 
 let lnglats = [
-  [102.54, 30.05],
-  [103.54, 30.95],
-  [104.54, 31.05],
-  [105.54, 30.95],
-  [104.54, 30.95],
+  [117.2, 39.13],
+  [106.27, 38.47],
+  [117.27, 31.86],
+  [117, 36.65],
+  [112.53, 37.87],
+  [121.48, 31.22],
+  [102.73, 25.04],
+  [111.65, 40.82],
+  [116.46, 39.92],
+  [125.35, 43.88],
+  [104.06, 30.67],
+  [104.94, 30.57],
+
+
+
+
 ];
 //初始化地图
 const mapInit = () => {
@@ -45,7 +75,7 @@ const mapInit = () => {
   });
 
   //信息窗口实例
-  // var infoWindow = new AMap.InfoWindow({ offset: new AMap.Pixel(0, -30) });
+  var infoWindow = new AMap.InfoWindow({ offset: new AMap.Pixel(0, -30) });
   //遍历生成多个标记点
   for (var i = 0, marker; i < data.length; i++) {
     let arr: any = [data[i][0], data[i][1]]
@@ -54,6 +84,12 @@ const mapInit = () => {
       position: arr,
       map: map,
     });
+    
+    marker.setLabel({
+      offset: new AMap.Pixel(1, 0), //设置文本标注偏移量
+      content: '<div>' + "劳资天下第一" + '</div>', //设置文本标注内容
+      direction: 'top', //设置文本标注方位
+    })
   }
   var polygon = new AMap.Polygon({
     path: lnglats,  //以5个点的坐标创建一个隐藏的多边形
@@ -66,11 +102,18 @@ const mapInit = () => {
   map.setFitView(overlaysList);
 }
 
+
+
+
+
+const danmus = ref([{ avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png', name: 'a', text: 'aaa' }, { avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png', name: 'b', text: 'bbb' }])
+const danmakuRef = ref(null)
 defineComponent({
   Drawer
 })
 onMounted(() => {
   mapInit();
+  // danmakuRef.value.play()
   // createSocket('ws://172.18.14.253:8007/dev-server/262/2p50atvb/websocket', (res) => {
   //   console.log(res);
 
@@ -78,4 +121,16 @@ onMounted(() => {
 });
 </script>
 <style lang="less">
+.vue-danmaku .danmus .dm.move {
+  z-index: 9999;
+}
+
+.amap-marker-label {
+  padding: 2px 6px;
+  border-radius: 5px;
+  box-shadow: 0 2px 6px 0 rgba(114, 124, 245, 0.5);
+  text-align: center;
+  font-size: 14px;
+  border: 0;
+}
 </style>
